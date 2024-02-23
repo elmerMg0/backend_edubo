2b399a1afd4c0284d2e2316dbf2fc15c88f50f94
elmerMg0 <mendozagutierrezelmer20@gmail.com>
2024-02-22 23:50:54 -0400

    plans by road and course added

--- models/Plan.php
+++ models/Plan.php
@@ -7,11 +7,17 @@ use Yii;
 /**
  * This is the model class for table "plan".
  *
- * @property string $id
  * @property string $nombre
  * @property int $precio_mes
  * @property bool $active
  * @property int $precio_total
+ * @property int $id
+ * @property string|null $duracion
+ * @property string|null $benefit
+ *
+ * @property Inscripcion[] $inscripcions
+ * @property RoadPlan[] $roadPlans
+ * @property RoadUser[] $roadUsers
  */
 class Plan extends \yii\db\ActiveRecord
 {
@@ -29,12 +35,13 @@ class Plan extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['id', 'nombre', 'precio_mes', 'active', 'precio_total'], 'required'],
-            [['id'], 'safe'],
+            [['nombre', 'precio_mes', 'active', 'precio_total'], 'required'],
             [['precio_mes', 'precio_total'], 'default', 'value' => null],
             [['precio_mes', 'precio_total'], 'integer'],
             [['active'], 'boolean'],
+            [['benefit'], 'string'],
             [['nombre'], 'string', 'max' => 20],
+            [['duracion'], 'string', 'max' => 15],
         ];
     }
 
@@ -44,11 +51,43 @@ class Plan extends \yii\db\ActiveRecord
     public function attributeLabels()
     {
         return [
-            'id' => 'ID',
             'nombre' => 'Nombre',
             'precio_mes' => 'Precio Mes',
             'active' => 'Active',
             'precio_total' => 'Precio Total',
+            'id' => 'ID',
+            'duracion' => 'Duracion',
+            'benefit' => 'Benefit',
         ];
+    }
+
+    /**
+     * Gets query for [[Inscripcions]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getInscripcions()
+    {
+        return $this->hasMany(Inscripcion::class, ['plan_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[RoadPlans]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getRoadPlans()
+    {
+        return $this->hasMany(RoadPlan::class, ['plan_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[RoadUsers]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getRoadUsers()
+    {
+        return $this->hasMany(RoadUser::class, ['plan_id' => 'id']);
     }
 }
